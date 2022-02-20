f4564c69fa485297b6bc39152e28b5df4fb6126b
Vikas Pandey <96241726+VikasBHU@users.noreply.github.com>
2022-02-20 22:09:47 +0530

    Digital Clock

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useEffect, useState } from "react";
+
 const App = () => {
+  const [date, setDate] = useState("");
+  useEffect(() => {
+    let time = new Date().toLocaleString();
+    setDate(time);
+    let tim = setInterval(() => {
+      let times = new Date().toLocaleString();
+      setDate(times);
+    }, 1000);
+    return () => {
+      clearInterval(tim);
+    };
+  }, []);
 
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time"> {date}</div>
     </div>
-  )
-}
-
-
+  );
+};
 export default App;
